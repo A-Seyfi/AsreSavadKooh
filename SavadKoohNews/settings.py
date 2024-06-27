c321cb61913e0106aa89ca2c917d438fcc856669
Emmett <sedighysajad2@gmail.com>
2024-06-27 16:29:06 +0330

    implementation of user app(register, active account, login, forget pass, reset pass, logout)

--- SavadKoohNews/settings.py
+++ SavadKoohNews/settings.py
@@ -117,3 +117,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.UserProfile'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'onionarchitecturemvc@gmail.com'
+EMAIL_HOST_PASSWORD = 'djangotoplearn@pass'
+EMAIL_PORT = 587
