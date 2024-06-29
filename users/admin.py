03d1ab7ab229ed97858b97f3fbd26d0c5e68df77
Emmett <sedighysajad2@gmail.com>
2024-06-29 23:36:53 +0330

    no changes

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import UserProfile
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "email"]
+    list_display = ["usernamefield", "password", "email"]
 
 admin.site.register(UserProfile, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
-    usernamefield = models.CharField(max_length=25, verbose_name='نام کاربری')
+    usernamefield = models.CharField(max_length=25, null=True,verbose_name='نام کاربری')
     password = models.CharField(max_length=50, verbose_name='رمز عبور')
     email = models.EmailField(max_length=100, verbose_name='ایمیل')
     email_active_code = models.CharField(max_length=100, verbose_name='کد فعالسازی ایمیل')
