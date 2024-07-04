ba6d5cb217c681ab84436a4e8a36118b1c04d02c
A-Seyfi <seyfiabolfazl64@gmail.com>
2024-07-04 13:52:05 +0330

    created gallery page

---
+++ static/js/gallery.js
@@ -0,0 +1,22 @@
+const active_pic = document.getElementById('active_pic')
+const active_title = document.getElementById('active_title')
+
+document.addEventListener('DOMContentLoaded', function () {
+    const gallery = document.querySelectorAll('.gallery__isle');
+
+    gallery.forEach(function (div) {
+      div.addEventListener('click', function () {
+        const pic = div.querySelector('img').src
+        const title = div.querySelector('.gallery_title').innerHTML
+
+        active_pic.src = pic
+        active_title.innerHTML = title
+
+        gallery.forEach(function (element) {
+          element.classList.remove('active');
+        });
+        div.classList.add('active');
+
+      });
+    });
+  });
