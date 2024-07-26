9408fe1d6a033ef19ac34e04b228d187f25aeed9
Emmett <sedighysajad2@gmail.com>
2024-07-26 23:34:12 +0330

    imagenews model & view

--- gallery/models.py
+++ gallery/models.py
@@ -7,8 +7,21 @@ class Gallery(models.Model):
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ثبت', null=True)
 
     class Meta:
+        verbose_name = 'گالری تصویر'
+        verbose_name_plural = 'گالری تصاویر'
+
+    def __str__(self):
+        return f"{self.title} - {self.create_date}"
+
+class ImageNews(models.Model):
+    image = models.ImageField(upload_to='static/images', verbose_name='تصویر', null=True, blank=True)
+    title = models.CharField(max_length=300, verbose_name='عنوان', null=True)
+    author = models.CharField(max_length=200, verbose_name='نویسنده')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ثبت', null=True)
+
+    class Meta:
         verbose_name = 'گزارش تصویری'
         verbose_name_plural = 'گزارش های تصویری'
 
     def __str__(self):
-        return f"{self.author} - {self.create_date}"
+        return f"{self.title} - {self.create_date}"
--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,13 @@
 function sendArticleComment(articleId) {
     var comment = $('#commentText').val();
-    console.log(comment);
-    $.get('add-article-comment', {
-        article_comment: comment,
-        article_id: articleId
-    }).then(res => {
-        console.log(res);
-        location.reload();
-    });
+    // console.log(comment);
+    if (comment) {
+        $.get('add-article-comment', {
+            article_comment: comment,
+            article_id: articleId
+        }).then(res => {
+            console.log(res);
+            location.reload();
+        });
+    }
 }
